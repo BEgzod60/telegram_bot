45b1c85b26136eea5987d2f2a2b66dfb34dec467
Begzod Mavlonov <begzodmavlonov250@gmail.com>
2025-12-05 18:31:56 +0500

    Add bot initialization in loaderr.py

--- kino_bot/loaderr.py
+++ kino_bot/loaderr.py
@@ -2,6 +2,7 @@ from aiogram import Bot, Dispatcher, types, Router, F
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 
+
 bot = Bot(
     token="8345524927:AAHas0HPcbEwph-gR_iS7py2iBEU5B5Mcg0",
     default= DefaultBotProperties(parse_mode=ParseMode.HTML)
